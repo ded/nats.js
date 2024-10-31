b60d784839aa982a52157fad97ea28082c0de32a
Dustin Diaz <polvero@gmail.com>
2024-10-31 12:56:54 -0700

    include comment for main entry point

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/* main entry point */
 export { connectNats, disconnectNats } from "./connection";
 export { consumeMessages, ConsumerOptions } from "./duro-consumer";
 export { publish } from "./duro-producer";
