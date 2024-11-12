089d5f44ce336e63b385fd1944adbe218e475342
ali-k-h <kosarh79@gmail.com>
2024-11-12 15:54:07 -0800

    create item created event dto for more felxibility

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ export { connectNats, disconnectNats } from "./connection";
 export { consumeMessages, ConsumerOptions } from "./duro-consumer";
 export { publish } from "./duro-producer";
 export type { MessageEnvelope } from "./interfaces";
-export { ItemEventDto } from "./event-dto/item.event.dto";
+export { ItemCreatedEventDto } from "./event-dto/item-created.event.dto";
