68cfb31862e1be4cb8f799715d6ff68a7be0758f
Ali Hosseinkhani <ali@Alis-MacBook-Pro.local>
2025-01-07 16:02:15 -0800

    clean up

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,4 @@ export type {
 export { ItemCreatedEventDto } from "./event-dto/item-created.event.dto";
 export { LibraryCreatedEventDto } from "./event-dto/libray-created.event.dto";
 export { ProcessedEvent } from "./services/processed-event.entity";
-export type { MessageInfo } from "./interfaces";
-export {
-  MessageDeduplicationService,
-  ProcessedEventCleanupService,
-} from "./services/mssage-deduplication.service";
+export { MessageDeduplicationService } from "./services/mssage-deduplication.service";
