d9ba34eebba8e980d10fd7dd0f98a36866a881d7
Ali Hosseinkhani <ali@Alis-MacBook-Pro.local>
2025-01-15 09:42:24 -0800

    import data room event dtos

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,6 @@ export { ItemCreatedEventDto } from "./event-dto/item-created.event.dto";
 export { LibraryCreatedEventDto } from "./event-dto/libray-created.event.dto";
 export { ProcessedEvent } from "./services/processed-event.entity";
 export { MessageDeduplicationService } from "./services/message-deduplication.service";
+export { DataroomCreatedEventDto } from "./event-dto/data-room-created.event.dto";
+export { DataroomRoleCreatedEventDto } from "./event-dto/data-room-role-created.event.dto";
+export { DataroomItemCreatedEventDto } from "./event-dto/data-room-item-created.event.dto";
